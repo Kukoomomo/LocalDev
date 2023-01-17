1d425dd0b59dd66eb3bfbd692f4c9314875a2c1a
kukoomomo <kukoomomo@gmail.com>
2023-01-17 21:31:40 +0800

    update tasks for sequencer add,delete，update

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ geth --datadir data1 init genesis.json
 geth --datadir data2 init genesis.json
 geth --datadir data3 init genesis.json
 
-geth --datadir data --password password.txt --unlock 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 --nodiscover --verbosity 4  --scheduler.address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+geth --datadir data --password password.txt --unlock 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 --nodiscover --verbosity 4  --scheduler.address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --scheduler.batchepoch 10
 
 geth --datadir data1 --rpcport 8085 --port 30306 --password password.txt --unlock 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 --nodiscover --verbosity 4 --scheduler.address 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 --sequencer.mode=true
 
@@ -29,7 +29,7 @@ admin.nodeInfo.enode
 net.peerCount
 admin.peers
 
-admin.addPeer("enode://d85c35c82fd58c7f3269ad3e33ac9f5ce2985732de36b01e31c6811d3feffec07a930e2827e291ca6328f96663173d9914c4c758299cb4c2cce2387ded87ece1@127.0.0.1:30303?discport=0")
+admin.addPeer("enode://8704732f1059123c982281a7e160741fbf7019df55a2190606d3eb1e8706bbea00897c2bb02790c76268a0b2ff62272c5b9bc5ba3d8fd961f5324dd523b56c38@127.0.0.1:30303?discport=0")
 
 eth.sendTransaction({from: eth.accounts[0], to: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", value: web3.toWei(1, "ether")})
 
@@ -38,3 +38,11 @@ eth.sendTransaction({from: eth.accounts[0], to: "0x70997970c51812dc3a010c7d01b50
 eth.sendTransaction({from: eth.accounts[0], to: "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc", value: web3.toWei(1, "ether")})
 ```
 
+```bash
+# delete sequencer 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+yarn hardhat withdrawAll --network l1
+# create sequencer 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC 
+yarn hardhat createSequencer --network l1
+# add deposit for sequencer 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC
+yarn hardhat deposit --network l1        
+```
--- Sequencer/tasks/sequencer.ts
+++ Sequencer/tasks/sequencer.ts
@@ -11,15 +11,15 @@ const deposit = ethers.utils.parseEther('1')
 const prvKeys = [
   '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
   '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
-  // '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
-  // '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6',
-  // '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a',
-  // '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba'
+  '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a',
+  '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6',
+  '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a',
+  '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba'
 ]
 const nodeIDs = [
   '0x7a6b7d46da49c9c5045a6ecb387415d7827506f1aff9bead32240772a39544d4',
-  '0xd4e8928003dd91c37eb6dfbef9d13b91a17b8e46a8208994153f8aade1597877',
-  // '0x2a1776580e7155022477c406dbd091983712d222d216d5e11d3c99b4e47671a8',
+  '0x93c6d59747ef03a1927f9d190a0038a4b64bdd77b7383b29244acce60c2ef23e',
+  '0x2a1776580e7155022477c406dbd091983712d222d216d5e11d3c99b4e47671a8',
   // '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6',
   // '0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a',
   // '0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba'
@@ -51,14 +51,14 @@ task("deploy")
     console.log("sequencer bit address :", await sequencer.bitToken())
 
     // create sequencer
-    for (var i = 1; i < wallets.length; i++) {
+    for (var i = 1; i < nodeIDs.length; i++) {
       await token.connect(wallets[i]).approve(sequencer.address, deposit)
       await sequencer.connect(wallets[i]).createSequencer(deposit, wallets[i].address, nodeIDs[i])
     }
     await sequencer.updateSequencerLimit(100)
 
     await sequencer.updateScheduler(wallets[0].address)
-    console.log("scheduler is :",await sequencer.scheduler())
+    console.log("scheduler is :", await sequencer.scheduler())
   });
 
 task("updateScheduler")
@@ -76,12 +76,56 @@ task("init")
   // .addParam("token")
   .setAction(async (taskArgs, hre) => {
     const tokenFactory = await hre.ethers.getContractFactory('BitTokenERC20')
-    const token = tokenFactory.attach("0xd778E3b2Ab28dd5D7a46e3bCF4D6d8e6eb716B57")
-    // 0xd778E3b2Ab28dd5D7a46e3bCF4D6d8e6eb716B57
+    const token = tokenFactory.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3")
     for (var i = 0; i < wallets.length; i++) {
 
-     var res  = await token.connect(wallets[i]).mint(amount)
-     console.log("res",res,"address:",wallets[i].addres)
+      var res = await token.connect(wallets[i]).mint(amount)
+      console.log("res", res, "address:", wallets[i].addres)
       // console.log("balance of", wallets[i].address, "is:", await token.balanceOf(wallets[i].address))
     }
+  });
+
+task("createSequencer")
+  .setAction(async (taskArgs, hre) => {
+    const sequencerFactory = await hre.ethers.getContractFactory('Sequencer')
+    const sequencer = sequencerFactory.attach("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0")
+    
+    const tokenFactory = await hre.ethers.getContractFactory('BitTokenERC20')
+    const token = tokenFactory.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+    
+    // // create sequencer
+    const useIndex = 2
+    await token.connect(wallets[useIndex]).mint(amount)
+    console.log("balances: ",await token.balanceOf(wallets[useIndex].address))
+    await token.connect(wallets[useIndex]).approve(sequencer.address, deposit)
+    console.log("allowance: ",await token.allowance(wallets[useIndex].address,sequencer.address))
+    await sequencer.connect(wallets[useIndex]).createSequencer(deposit, wallets[useIndex].address, nodeIDs[useIndex])
+  });
+
+task("withdrawAll")
+ .setAction(async (taskArgs, hre) => {
+    const sequencerFactory = await hre.ethers.getContractFactory('Sequencer')
+    const sequencer = sequencerFactory.attach("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0")
+    // const sequencer = sequencerFactory.attach("0xBe0F340075060F856612d91e17cAe599dE92C745")
+
+    // 0xBe0F340075060F856612d91e17cAe599dE92C745
+    // // create sequencer
+    const useIndex = 2
+    console.log(wallets[useIndex].address)
+    await sequencer.connect(wallets[useIndex]).withdrawAll()
+  });
+
+task("deposit")
+ .setAction(async (taskArgs, hre) => {
+    const sequencerFactory = await hre.ethers.getContractFactory('Sequencer')
+    const sequencer = sequencerFactory.attach("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0")
+    const tokenFactory = await hre.ethers.getContractFactory('BitTokenERC20')
+    const token = tokenFactory.attach("0x5FbDB2315678afecb367f032d93F642f64180aa3")
+    
+    // 0xBe0F340075060F856612d91e17cAe599dE92C745
+    // // create sequencer
+    const useIndex = 2
+    await token.connect(wallets[useIndex]).mint(deposit)
+    await token.connect(wallets[useIndex]).approve(sequencer.address, deposit)
+    await sequencer.connect(wallets[useIndex]).deposit(deposit)
   });
